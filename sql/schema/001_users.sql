2fd100174a1bbefa121101ba137d5181cfc9cb9b
kalin chakma <kalin@mediusware.com>
2025-01-28 13:27:31 +0600

    updated user model

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,7 +3,9 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    email VARCHAR NOT NULL,
+    password VARCHAR NOT NULL
 );
 
 -- +goose Down
